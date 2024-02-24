5419b1ec43fbc1538cc7e43df47b7cdff41a7f7f
Priyanshu Singh <priy2032004anshu@gmail.com>
2024-02-24 23:51:47 +0530

    update

---
+++ SymmetricDifference.py
@@ -0,0 +1,23 @@
+# x=['1','2','3']
+# print(x[1]+x[2])
+# x=[1,2,3,4,5]
+# print(x[0::2])
+# data=[1,2,3,4]
+# data[3]=7
+# bb=data.copy()
+# print(bb)
+# print("abc. DEF".capitalize())
+# s="learn Python"
+# print(s.index("n")+s.rindex("n"))
+# i=1
+# while True:
+#     if i % 3==0:
+#         break
+#     print(i)
+#     i+=1
+# list=range(100,110)
+# print(list.index(105))
+fruits = [4, 55, 64, 32, 16, 32]
+
+x = fruits.index(32)
+print(x)
